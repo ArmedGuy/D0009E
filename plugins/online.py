c1b289db1c8ee1d37a261e75287a9df141290af8
Johan Jatko <johan.jatko@outlook.com>
2017-10-31 01:21:49 +0100

    Update online.py

--- plugins/online.py
+++ plugins/online.py
@@ -12,16 +12,18 @@ class Online(PluginBase):
 	def handleOnline(self, bot, channel, params):
 		bot.sendMessage("PRIVMSG", channel, self.getOnline)
     
-    def getOnline(self, server):
-        try:
-            url = "http://www.ludd.ltu.se/~armedguy/users_online.php"
-            f = urllib.request.urlopen(url)
-            data = f.read().decode('utf-8')
-            json = json.reads(data)
-            if server not in json:
-                return "Server not found"
-            else:
-                return "Users online on %s: %s" % (server, ", ".join(json[server]))
+	def getOnline(self, server):
+		try:
+			url = "http://www.ludd.ltu.se/~armedguy/users_online.php"
+			f = urllib.request.urlopen(url)
+			data = f.read().decode('utf-8')
+			json = json.reads(data)
+			if server not in json:
+				return "Server not found"
+			else:
+				return "Users online on %s: %s" % (server, ", ".join(json[server]))
+		except:
+			return "Could not get online users"
             
 
 mainclass = Online
